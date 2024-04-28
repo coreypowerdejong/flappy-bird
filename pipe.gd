0739a731a34b53267e29d3969b90e01fbe0a5b30
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-04-28 15:39:00 +0100

    Pipe wraparound is now based on window size instead of magic numbers

--- pipe.gd
+++ pipe.gd
@@ -1,15 +1,18 @@
 extends Node2D
 
+var window_size
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = 400 + $top_pipe/CollisionShape2D.shape.size.x / 2
+	window_size = get_viewport().get_visible_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x -= 200*delta
 	if global_position.x + $top_pipe/CollisionShape2D.shape.size.x / 2 < 0:
-		global_position.x += 400 + $top_pipe/CollisionShape2D.shape.size.x
-		global_position.y = randi_range(-0, 320)
+		global_position.x += window_size.x + $top_pipe/CollisionShape2D.shape.size.x
+		global_position.y = randi_range(0, window_size.y - 400)
 
 
