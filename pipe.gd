76b3fa922e0a17f5524940e8847e3bcf15fba330
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-04-28 11:00:17 +0100

    Add pipe random vertical offset

--- pipe.gd
+++ pipe.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
-var window_size = get_viewport()
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#position.x = 1280
-	pass
+	position.x = 400 + $top_pipe/CollisionShape2D.shape.size.x / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +10,6 @@ func _process(delta):
 	global_position.x -= 200*delta
 	if global_position.x + $top_pipe/CollisionShape2D.shape.size.x / 2 < 0:
 		global_position.x += 400 + $top_pipe/CollisionShape2D.shape.size.x
+		global_position.y = randi_range(-0, 320)
 
 
