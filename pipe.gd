aa84356c23d4f849fe0f0aa941922235103f6fe8
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-05-01 22:23:39 +0100

    Add proper pipe texture

--- pipe.gd
+++ pipe.gd
@@ -6,7 +6,7 @@ signal score_point
 
 func reset():
 		window_size = get_viewport().get_visible_rect().size
-		position.x = window_size.x + $top_pipe/CollisionShape2D.shape.size.x / 2
+		position.x = window_size.x + $top_pipe/Head.shape.size.x / 2
 		passed_pipe = false
 
 # Called when the node enters the scene tree for the first time.
@@ -17,8 +17,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position.x -= 200*delta
-	if global_position.x + $top_pipe/CollisionShape2D.shape.size.x / 2 < 0:
-		global_position.x += window_size.x + $top_pipe/CollisionShape2D.shape.size.x
+	if global_position.x + $top_pipe/Head.shape.size.x / 2 < 0:
+		global_position.x += window_size.x + $top_pipe/Head.shape.size.x
 		global_position.y = randi_range(0, window_size.y - 400)
 		passed_pipe = false
 	
