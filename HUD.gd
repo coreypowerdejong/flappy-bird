e6c3f71a3346eb0e272f14f68cb9dabb782dc6fe
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-05-01 18:08:44 +0100

    Add game over message and game pause when colliding

--- HUD.gd
+++ HUD.gd
@@ -1,12 +1,15 @@
 extends CanvasLayer
 
 signal start_game
+signal pause
 
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	score = 0
 	$ScoreLabel.text = str(score)
+	$GameOverLabel.hide()
+	pause.emit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,8 +24,15 @@ func _on_pipe_score_point():
 
 func _on_start_button_pressed():
 	$StartButton.hide()
+	$GameOverLabel.hide()
+	$ScoreLabel.show()
 	start_game.emit()
 
 
 func _on_pipe_body_entered(body):
-	print("Hit!")
+	$StartButton.show()
+	$StartButton.text = "Play Again"
+	$GameOverLabel.text = "Game over!\nScore: " + str(score)
+	$ScoreLabel.hide()
+	$GameOverLabel.show()
+	pause.emit()
