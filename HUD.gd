bb88fe42cb1a87e242ad36d80ccecff5fab06713
Corey Power De Jong <corey.powerdejong@ucdconnect.ie>
2024-05-10 15:23:23 +0100

    Add high score feature

--- HUD.gd
+++ HUD.gd
@@ -4,6 +4,7 @@ signal start_game
 signal pause
 
 var score
+var high_score
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	score = 0
@@ -28,10 +29,24 @@ func _on_start_button_pressed():
 
 
 func _on_pipe_body_entered(_body):
+	high_score = int(load_score())
+	high_score = max(score, high_score)
+	save_score(high_score)
 	$LoseSound.play()
 	$StartButton.show()
 	$StartButton.text = "Play Again"
-	$GameOverLabel.text = "Game over!\nScore: " + str(score)
+	$GameOverLabel.text = "Game over!\nScore: " + str(score) + "\nHigh Score: " + str(high_score)
 	$ScoreLabel.hide()
 	$GameOverLabel.show()
 	pause.emit()
+
+func save_score(content):
+	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
+	file.store_string(str(content))
+
+func load_score():
+	var file = FileAccess.open("user://save_game.dat", FileAccess.READ)
+	if file == null:
+		return 0
+	var content = file.get_as_text()
+	return content
